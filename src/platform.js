0e9884b4a4e205f57f22ee59b55c41805d186e6a
Kurt Warwick <you@example.com>
2020-06-30 23:00:09 +0200

    Fixes

--- src/platform.js
+++ src/platform.js
@@ -83,12 +83,8 @@ class Platform {
             let accessory = this.accessories[accessoryIndex];
             let accessoryFound = false;
 
-            this.debug(accessory.context);
-
             for(let deviceIndex = 0; deviceIndex < devices.length; deviceIndex ++) {
                 let device = devices[deviceIndex];
-
-                this.debug(device);
 
                 if(accessory.context.uid === device.parsedCredentials.uniqueIdentifier) {
                     accessoryFound = true;
