de87855339471b5aeb3c92da3fe5e7bfca798bd3
Kurt Warwick <you@example.com>
2020-06-30 21:05:56 +0200

    Fixes

--- src/accessory.js
+++ src/accessory.js
@@ -59,14 +59,13 @@ class Accessory {
 
     onDeviceInfo(message) {
         this.power = message.payload.logicalDeviceCount == 1;
-
-        this.services && this.services.map(service => service.getCharacteristic(this.platform.api.hap.Characteristic.Active).updateValue(this.power));
+        this.service && this.service.getCharacteristic(this.platform.api.hap.Characteristic.On).updateValue(this.power);
     }
 
-    async onPower(value, next, service) {
+    async onPower(value, next) {
         clearTimeout(this.powerTimer);
 
-        this.platform.debug(`turning ${service.type} service for accessory (${this.device.name} [${this.device.uid}]) ${value ? "on" : "off"}.`);
+        this.platform.debug(`turning ${this.service.type} service for accessory (${this.device.name} [${this.device.uid}]) ${value ? "on" : "off"}.`);
 
         if (value && !this.power) {
             await this.device.sendKeyCommand(appletv.AppleTV.Key.LongTv);
