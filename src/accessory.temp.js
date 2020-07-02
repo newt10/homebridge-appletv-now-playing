b22887cc527667244d19dec5d50205748456b879
Kurt Warwick <you@example.com>
2020-07-02 17:05:30 +0200

    Testing

--- src/accessory.temp.js
+++ src/accessory.temp.js
@@ -1,37 +1,71 @@
 const Accessory = require("./accessory");
+const { platformName } = require("./platform.temp");
+
+const AccessoryModel = "Apple TV";
+const AccessoryManufacturer = "Apple";
 
 module.exports = class TempAccessory {
     constructor(platform, config, device) {
-        let accessoryUid = platform.api.hap.uuid.generate(`${device.uid}`);
-        let accessory = new platform.api.platformAccessory(config.name, accessoryUid);
+        this.configureAccessory = this.configureAccessory.bind(this);
+        this.configureServices = this.configureServices.bind(this);
+        this.configureSwitchService = this.configureSwitchService.bind(this);
 
-        this.on = false;
+        this.setPower = this.setPower.bind(this);
+        this.getPower = this.getPower.bind(this);
+        
+        this.platform = platform;
+        this.config = config;
+        this.device = device;
 
-        accessory
-            .getService(platform.api.hap.Service.AccessoryInformation)
-            .setCharacteristic(platform.api.hap.Characteristic.Manufacturer, "Apple")
-            .setCharacteristic(platform.api.hap.Characteristic.Model, "Apple TV")
-            .setCharacteristic(platform.api.hap.Characteristic.SerialNumber, device.uid)
-            .setCharacteristic(platform.api.hap.Characteristic.Name, config.name);
+        this.on = false; 
 
-        let service = accessory.getService(platform.api.hap.Service.Switch);
+        this.configureAccessory(config, device);
+    }
 
-        if (!service) {
-            service = accessory.addService(platform.api.hap.Service.Switch, `${config.name} Switch`, `${accessory.context.uid}_switch`);
+    log(message) {
+        this.platform.log(`[${this.config.name}] ${message}`);
+    }
+
+    configureAccessory() {
+        this.uid = this.platform.api.hap.uuid.generate(`${device.uid}_apple_tv`);
+        this.instance = new platform.api.platformAccessory(config.name, this.uid);
+
+        this.configureServices();
+
+        this.platform.registerAccessory(this.instance);
+    }
+
+    configureServices() {
+        this.instance
+            .getService(this.platform.api.hap.Service.AccessoryInformation)
+            .setCharacteristic(this.platform.api.hap.Characteristic.Manufacturer, AccessoryManufacturer)
+            .setCharacteristic(this.platform.api.hap.Characteristic.Model, AccessoryModel)
+            .setCharacteristic(this.platform.api.hap.Characteristic.SerialNumber, this.device.uid)
+            .setCharacteristic(this.platform.api.hap.Characteristic.Name, this.config.name);
+
+        this.configureSwitchService();
+    }
+
+    configureSwitchService() {
+        this.switchService = this.instance.getService(this.platform.api.hap.Service.Switch);
+
+        if (!this.switchService) {
+            this.switchService = this.instance.addService(this.platform.api.hap.Service.Switch, `${this.config.name} Switch`, `${this.uid}_switch`);
         }
 
-        service
-            .getCharacteristic(platform.api.hap.Characteristic.On)
-            .on("get", (callback) => {
-                platform.log(`requesting on characteristic => ${this.on}`);
-                callback(null, this.on);
-            })
-            .on("set", (value, callback) => {
-                platform.log(`setting on characteristic => ${value}`);
-                this.on = value;
-                callback(null);
-            });
+        this.switchService.getCharacteristic(platform.api.hap.Characteristic.On).on("get", this.getPower).on("set", this.setPower);
+    }
 
-        platform.registerAccessory(accessory);
+    setPower(value, callback) {
+        this.log(`setting on characteristic => ${value}`);
+        this.on = value;
+
+        callback(null);
+    }
+
+    getPower(callback) {
+        this.log(`requesting on characteristic => ${this.on}`);
+
+        callback(null, this.on);
     }
 };
