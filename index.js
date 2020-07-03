73be861db32f49eef1702a9a650fe7518edda9b8
Kurt Warwick <you@example.com>
2020-07-03 10:53:41 +0200

    Testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const Platform = require('./src/platform.temp');
+const Platform = require('./src/platform');
 
 module.exports = function (homebridge) {
     homebridge.registerPlatform("homebridge-appletv-now-playing", "AppleTvNowPlayingPlatform", Platform, true);
