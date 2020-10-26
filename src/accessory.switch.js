fcb9c0c66fe78f8cf428ea8f77965ea17c02cf9b
kurtwarwick-new <61435184+kurtwarwick-new@users.noreply.github.com>
2020-10-26 13:05:24 +0200

    Update accessory.switch.js

--- src/accessory.switch.js
+++ src/accessory.switch.js
@@ -205,6 +205,7 @@ module.exports = class SwitchAccessory {
     onNowPlaying(message) {
         try {
             this.debug("received now plaing information");
+            this.debug(message);
 
             if (message && message.playbackState && message.playbackState.length > 1) {
                 message.playbackState = message.playbackState[0].toUpperCase() + message.playbackState.substring(1).toLowerCase();
